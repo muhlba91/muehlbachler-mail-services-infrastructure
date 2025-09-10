8203451225442a45eec9c211359e55b7f8b6328a
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2025-09-10 13:15:11 +0200

    feat: install jq as pre-requisite of mailcow

--- assets/mailcow/prepare.sh
+++ assets/mailcow/prepare.sh
@@ -6,3 +6,7 @@ mkdir -p /opt/backup/mailcow || true
 
 # clone mailcow
 git clone https://github.com/mailcow/mailcow-dockerized /opt/mailcow || true
+
+# install pre-requisites
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install --yes jq
